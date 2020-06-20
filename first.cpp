2ffe7d3d696f3e63afc77a9ff0d55995d873e825
masterShanto <mastershanto@gmail.com>
2020-06-21 01:32:10 +0600

    real & main file is now install for cpp.cpp

--- first.cpp
+++ first.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-{
+{   ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
     int n, marks[n];
     cout << "Enter the student marks: ";
     cin >> n;
@@ -24,22 +25,5 @@ int main()
     }
 
     
-    for (int i = 0; i < n; ++i)
-    {
-        for (int i = 0; i < i + 1; ++i)
-        {
-            cout << "* ";
-        }
-        cout << "\n";
-    }
-    for (int i = 0; i < n; ++i)
-    {
-        cout << "The student's marks[" << i << "]= ";
-        cin >> marks[i];
-    }
-    for (int i = 0; i < n; ++i)
-    {
-        cout << "Mark of student[" << i << "] is: " << marks[i] << endl;
-    }
     getch();
 }
